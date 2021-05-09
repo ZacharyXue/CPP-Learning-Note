f4673d6a5050fcba3d60395733ed1729b7bc46fd
Zach <xuezihao2016@outlook.com>
2021-05-09 20:10:56 +0800

    learning 4.4

---
+++ Chapter 04/ex4.13.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i;
+    double j;
+    j = i = 3.5;
+    cout<< "The value of i is "<< i << endl;
+    cout<< "The value of j is "<< j << endl;
+    return 0;
+}
